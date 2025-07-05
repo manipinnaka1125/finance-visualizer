37d065d010010f06ac3b9b4096d1299de6f06379
manipinnaka1125 <2200030449@kluniversity.in>
2025-07-05 13:08:27 +0530

    database

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(process.env.MONGODB_URI!);
+
+  await mongoose.connect(process.env.MONGODB_URI!, {
+    dbName: 'Financedbs', // specify your database name explicitly
+  });
 };
 
 export default connectDB;
